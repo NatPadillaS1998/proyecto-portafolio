fd17a5f6351002f730d74ffef182840d3fd362c7
NatPadillaS1998 <60945005+NatPadillaS1998@users.noreply.github.com>
2020-05-19 12:23:59 -0500

    19 mayo

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 // TODO Scripts
+const menu = document.querySelector(".menu-toggle");
+
+const menuList = menu.querySelector(".menu-list");
+
+menu.addEventListener("click", menuToggler);
+
+function menuToggler() {
+  menuList.toggleAttribute("hidden");
+}
